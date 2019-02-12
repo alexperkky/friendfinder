87ab5c8a7f99649f740061ef9922ea36045ea748
Alexperkky <alex.perkinson5@gmail.com>
2019-02-12 15:06:01 -0800

    Fully working, final commit

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,16 @@
+// Load friend object data
 var friends = require("../data/friends");
 
-module.exports = function(app) {
 
+
+module.exports = function(app) {
+ 
+    // GET Routes
   app.get("/api/friends", function(req, res) {
     res.json(friends);
   });
+
+    // POST requests
 
   app.post("/api/friends", function(req, res) {
   
@@ -14,42 +20,41 @@ module.exports = function(app) {
       friendDifference: Infinity
     };
 
-    
+    // Get user input data from survey
     var userData = req.body;
     var userScores = userData.scores;
 
-    
+    // Calculates sum of survey scores from all responses
     var totalDifference;
 
-    
+    // Loop through friends
     for (var i = 0; i < friends.length; i++) {
       var currentFriend = friends[i];
       totalDifference = 0;
 
       console.log(currentFriend.name);
 
-      
+      // We then loop through all the scores of each friend
       for (var j = 0; j < currentFriend.scores.length; j++) {
         var currentFriendScore = currentFriend.scores[j];
         var currentUserScore = userScores[j];
 
-        
+        // We calculate the difference between the scores and sum them into the totalDifference
         totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
       }
 
-      
       if (totalDifference <= bestMatch.friendDifference) {
-        
+        // Reset the bestMatch to be the new friend.
         bestMatch.name = currentFriend.name;
         bestMatch.photo = currentFriend.photo;
         bestMatch.friendDifference = totalDifference;
       }
     }
 
-    
+   
     friends.push(userData);
 
-    
+   
     res.json(bestMatch);
   });
 };
--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+// Dependencies
+
 var express = require("express");
 var bodyParser = require("body-parser");
+
+// Creates express server
 var app = express();
+
+// Set up a PORT
 var PORT = process.env.PORT || 8080;
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
+
+// Router
+
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
+
+
+// Starts server
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
